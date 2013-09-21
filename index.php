8c67b8f832fb01ab1f708b0cf164fc8b17577122
Zach <zgreene02@gmail.com>
2013-09-20 21:44:41 -0400

    Work towards AJAX, move a chunk of PHP, etc.
    Work in progress.

--- index.php
+++ index.php
@@ -11,6 +11,23 @@
     	<![endif]-->
     	<script src="./includes/bootstrap.js"></script>
     	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    	<!-- Alrighty, let's make some AJAX. -->
+    	<script type="text/javascript">
+    	function beamMeUpScotty()
+    	{
+    	var sendit;
+    	if (window.XmlHttpRequest)
+    	{
+    	sendit = new XmlHttpRequest();
+    	}
+    	else
+    	{
+    	sendit = new ActiveXObject("Microsoft.XMLHTTP");
+    	}
+    	sendit.open("POST","crypt.php",true);
+   	sendit.send("encrypt=document.forms["encrypt"].elements["encrypt"].value");
+    	}
+    	</script>
 </head>
 
 <body onLoad="document.encrypt.encrypt.focus();">
@@ -20,79 +37,24 @@
 		<p>Welcome to the obfuscator. Please enter text to encrypt or decrypt, and a password.</p>
 		</div>
 		<br />
-                <p>
-                <?php
-$encrypted = "";
-$decrypted = "";
-$operation = "";
-$password = "";
-$invalid = FALSE;
-if (isset($_POST['encrypt']))
-{ $decrypted = $_POST['encrypt']; }
-if (isset($_POST['decrypt']))
-{ $encrypted = $_POST['decrypt']; }
-if (isset($_POST['password']))
-{ $password = $_POST['password']; }
-                    // echo "key: ".$password."</br>";
-                    // echo "encrypted: ".$encrypted."<br/>";
-                    // echo "decrypted: ".$decrypted."<br/>";
-if (is_null($encrypted))
-{ $encrypted = ""; }
-if (is_null($decrypted))
-{ $decrypted = ""; }
-if (strlen($decrypted)>0 && strlen($encrypted)>0) // don't know if I need to encrypt or decrypt
-{ $invalid = TRUE; }
-if (strlen($decrypted)<1 && strlen($encrypted)<1) // nothing was entered
-{ $invalid = TRUE; }
-if (!$invalid)
-{
-    if (strlen($decrypted) > strlen($encrypted))
-    { $operation = "E"; }
-    else
-    { $operation = "D"; }
-}
-                if ($invalid)
-                { 
-                    echo "Invalid.  Text box likely empty?<br/>";
-                    // echo "key: ".$password."</br>";
-                    // echo "encrypted: ".$encrypted."<br/>";
-                    // echo "decrypted: ".$decrypted."<br/>";
-                    //$encrypted = "Text to decrypt:";
-                    //$decrypted = "Text to encrypt:";
-              }
-                else
-                {
-                // echo "key: ".$password."<br/>";
-                // echo "md5 of key: ".md5($password)."<br/>";
-                if ($operation == "D")
-                { $decrypted = decrypt(trim($encrypted), trim($password)); }
-                else
-                { $encrypted = encrypt(trim($decrypted), trim($password)); }
-                // echo "encrypted: ".$encrypted."<br/>";
-                // echo "decrypted: ".$decrypted."<br/>";
-                if (strlen($password)<3)
-                { echo "<div class=\"alert\"><strong>Warning:</strong> Password is too short.  It is ".strlen($password)." characters long.</div><br/>"; }
-                }
-                ?>
-                </p>
-		<form role="form" name="encrypt" action="" method="post">
+		<form role="form" name="encrypt">
 			<div class="form-group">
 			<textarea class="form-control" id="encrypt" name="encrypt" placeholder="Text to encrypt"><?php echo $decrypted; ?></textarea>
 			<label for="password">Password:</label>
 			<div class="col-lg-10">
 			<input type="password" class="form-control" id="password" name="password" value="<?php echo $password; ?>">
 			</div>
-			<button type="submit" class="btn btn-primary">Encrypt</button>
+			<button type="submit" data-loading-text="Loading..." class="btn btn-primary">Encrypt</button>
 			</div>
 		</form>
-		<form role="form" name="decrypt" action="" method="post">
+		<form role="form" name="decrypt">
 			<div class="form-group">
 			<textarea class="form-control" id="decrypt" name="decrypt" placeholder="Text to decrypt"><?php echo $encrypted; ?></textarea>
 			<label for="password">Password:</label>
 			<div class="col-lg-10">
 			<input type="password" class="form-control" id="password" name="password" value="<?php echo $password; ?>">
 			</div>
-			<button type="submit" class="btn btn-primary">Decrypt</button>
+			<button type="submit" data-loading-text="Loading..." class="btn btn-primary">Decrypt</button>
 			</div>
 		</form>
         </div>
@@ -102,66 +64,3 @@ if (!$invalid)
 </body>
 
 </html>
-
-<?php
-function encrypt($decrypted, $hash)
-{
-    $hash = md5($hash);
-    $output = "";
-    $index = 0;
-    foreach(str_split($decrypted) as $char)
-    {
-        if ($index >= strlen($hash)) { $index = 0; }
-        $byte = ord(substr($hash,$index,1));
-        $index++;
-        // echo " ".chr($byte)." "; //debug
-        if ($byte < 58) { $byte -= 47; } // convert ascii numbers into number 1 - 10
-        if ($byte > 64) { $byte -= 86; } // convert ascii hex a-f into numbers 11-16
-        // echo " ".$byte." "; //debug
-        $upperCase="";
-        $tempchar = ord($char);
-        // echo chr($tempchar).">"; //debug
-        if ($tempchar > 47 && $tempchar < 58) { $upperCase = "N"; } // number
-        if ($tempchar > 64 && $tempchar < 91) { $upperCase = "U"; } // upper case
-        if ($tempchar > 96 && $tempchar < 123) { $upperCase = "L"; } // lower case
-        if (strlen($upperCase) > 0 ) { $tempchar += $byte; } // change the character if a number or letter
-        if ($upperCase == "N" && $tempchar > 57) { $tempchar -= 10; } // if it wraps out of range of a number
-        if ($upperCase == "N" && $tempchar > 57) { $tempchar -= 10; } // repeat once
-        if ($upperCase == "U" && $tempchar > 90) { $tempchar -= 26; }
-        if ($upperCase == "L" && $tempchar > 122) { $tempchar -= 26; }
-        // echo $upperCase.chr($tempchar); //debug
-        $output = $output.chr($tempchar);
-    }
-    return $output;
-}
-function decrypt($encrypted, $hash)
-{
-    $hash = md5($hash);
-    $output = "";
-    $index = 0;
-    foreach(str_split($encrypted) as $char)
-    {
-        if ($index >= strlen($hash)) { $index = 0; }
-        $byte = ord(substr($hash,$index,1));
-        $index++;
-        // echo " ".chr($byte)." "; //debug
-        if ($byte < 58) { $byte -= 47; } // convert ascii numbers into number 1 - 10
-        if ($byte > 64) { $byte -= 86; } // convert ascii hex a-f into numbers 11-16
-        // echo " ".$byte." "; //debug
-        $upperCase="";
-        $tempchar = ord($char);
-        // echo chr($tempchar).">"; //debug
-        if ($tempchar > 47 && $tempchar < 58) { $upperCase = "N"; } // number
-        if ($tempchar > 64 && $tempchar < 91) { $upperCase = "U"; } // upper case
-        if ($tempchar > 96 && $tempchar < 123) { $upperCase = "L"; } // lower case
-        if (strlen($upperCase) > 0 ) { $tempchar -= $byte; } // change the character if a number or letter
-        if ($upperCase == "N" && $tempchar < 48) { $tempchar += 10; } // if it wraps out of range of a number
-        if ($upperCase == "N" && $tempchar < 48) { $tempchar += 10; } // repeat once
-        if ($upperCase == "U" && $tempchar < 65) { $tempchar += 26; }
-        if ($upperCase == "L" && $tempchar < 97) { $tempchar += 26; }
-        // echo $upperCase.chr($tempchar); //debug
-        $output = $output.chr($tempchar);
-    }
-    return $output;
-}
-?>
